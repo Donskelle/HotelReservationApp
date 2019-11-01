23ce32fac8dfb2e17cd700d06c95ccbb63d975df
Fabian Puszies <donskelle@googlemail.com>
2019-11-01 21:28:14 +0100

    Implement Suspense Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,56 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import loadable from '@loadable/component';
 
 import Menu from './Menu';
 import Wrapper from './Wrapper';
 import PrivateRoute from './PrivateRoute';
 import '../styles/css-reset.css';
 
-const LazyLogin = loadable(() => import('../pages/Login'));
-const LazyDashboard = loadable(() => import('../pages/Dashboard'));
-const LazyReservationCreate = loadable(() =>
+const LazyLogin = React.lazy(() => import('../pages/Login'));
+const LazyDashboard = React.lazy(() => import('../pages/Dashboard'));
+const LazyReservationCreate = React.lazy(() =>
   import('../pages/ReservationCreate')
 );
-const LazyReservationList = loadable(() => import('../pages/ReservationList'));
-const LazyReservationEdit = loadable(() => import('../pages/ReservationEdit'));
-const LazyRoomList = loadable(() => import('../pages/RoomList'));
+const LazyReservationList = React.lazy(() =>
+  import('../pages/ReservationList')
+);
+const LazyReservationEdit = React.lazy(() =>
+  import('../pages/ReservationEdit')
+);
+const LazyRoomList = React.lazy(() => import('../pages/RoomList'));
 
 function App() {
   return (
     <Router>
       <Wrapper>
         <Menu />
-        <Switch>
-          <PrivateRoute path="/dashboard">
-            <LazyDashboard fallback={<h1>Loading</h1>} />
-          </PrivateRoute>
-          <PrivateRoute
-            path={[
-              '/rooms/:id/reservations/create',
-              '/rooms/createreservation'
-            ]}
-          >
-            <LazyReservationCreate fallback={<h1>Loading</h1>} />
-          </PrivateRoute>
-          <PrivateRoute path="/rooms/:id/reservations/:reservationId/edit">
-            <LazyReservationEdit fallback={<h1>Loading</h1>} />
-          </PrivateRoute>
-          <PrivateRoute path="/rooms/:id/reservations">
-            <LazyReservationList fallback={<h1>Loading</h1>} />
-          </PrivateRoute>
-          <PrivateRoute path="/rooms">
-            <LazyRoomList fallback={<h1>Loading</h1>} />
-          </PrivateRoute>
-          <Route path="/">
-            <LazyLogin fallback={<h1>Loading</h1>} />
-          </Route>
-        </Switch>
+        <Suspense fallback={'<h2>Loading</h2>'}>
+          <Switch>
+            <PrivateRoute path="/dashboard">
+              <LazyDashboard />
+            </PrivateRoute>
+            <PrivateRoute
+              path={[
+                '/rooms/:id/reservations/create',
+                '/rooms/createreservation'
+              ]}
+            >
+              <LazyReservationCreate fallback={<h1>Loading</h1>} />
+            </PrivateRoute>
+            <PrivateRoute path="/rooms/:id/reservations/:reservationId/edit">
+              <LazyReservationEdit fallback={<h1>Loading</h1>} />
+            </PrivateRoute>
+            <PrivateRoute path="/rooms/:id/reservations">
+              <LazyReservationList fallback={<h1>Loading</h1>} />
+            </PrivateRoute>
+            <PrivateRoute path="/rooms">
+              <LazyRoomList fallback={<h1>Loading</h1>} />
+            </PrivateRoute>
+            <Route path="/">
+              <LazyLogin />
+            </Route>
+          </Switch>
+        </Suspense>
       </Wrapper>
     </Router>
   );
