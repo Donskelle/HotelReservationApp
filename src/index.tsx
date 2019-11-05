c4eebb42ea5f98a42d819610b4688850518da834
Fabian Puszies <donskelle@googlemail.com>
2019-11-05 11:56:30 +0100

    Add Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+(ReactDOM as any).createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
